28b559b774d26f816e581dc6ee99458bc7b081d4
u1yuvan <yuvanraj700@gmail.com>
2023-09-02 12:22:01 +0530

    JavaScript ARRAY

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 // var - old way of saving variables
 // let - modern way of saving varables(we can modify or edit value any time)
 // const - we can't change the value (permanent value we can't edit or modify)
-
+                                    //VARIABLES
+// {object}
+// [array]
 
 // let name = "yuvan";
 // name ="YUVAN"
@@ -15,19 +17,30 @@
 // console.log(msg, DOB);
 
 
+ ///////////////////////////////////////OBJECT
+// let laptopSpecifications= {
+//     Brand:"HP",
+//     Model:"hp15s 15inch",
+//     Processor:"Ryzen 5700u",
+//     RamOptions:{
+//         1:"8gb",
+//         2:"16gb"
+//     },
+//     KeyBoardType:"Chicklet keyboard with backlit",
+//     Rom:"500gb",
 
-let laptopSpecifications= {
-    Brand:"HP",
-    Model:"hp15s 15inch",
-    Processor:"Ryzen 5700u",
-    RamOptions:{
-        1:"8gb",
-        2:"16gb"
-    },
-    KeyBoardType:"Chicklet keyboard with backlit",
-    Rom:"500gb",
+
+// }
+
+// console.log(laptopSpecifications)
 
 
-}
 
-console.log(laptopSpecifications)
+
+                                                  //ARRAY
+
+let colors =["Blue", "Black", "Red","Green"]
+colors[5] = "white"
+
+console.log(colors);
+
