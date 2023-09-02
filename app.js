949b9f70eb248a88c46ab95ee88d14a5a359a14b
u1yuvan <yuvanraj700@gmail.com>
2023-09-02 12:09:27 +0530

    JavaScript Objects

--- app.js
+++ app.js
@@ -2,12 +2,32 @@
 // var - old way of saving variables
 // let - modern way of saving varables(we can modify or edit value any time)
 // const - we can't change the value (permanent value we can't edit or modify)
-let name = "yuvan";
-name ="YUVAN"
-const DOB ="10-10-2002"//we can use const variable like this permanent value
-let msg = "hello my name is " + name + " im Building JS i was born in "
-// alert
-alert(msg);
 
-// logging
-console.log(msg, DOB);
+
+// let name = "yuvan";
+// name ="YUVAN"
+// const DOB ="10-10-2002"//we can use const variable like this permanent value
+// let msg = "hello my name is " + name + " im Building JS i was born in "
+// // alert
+// alert(msg);
+
+// // logging
+// console.log(msg, DOB);
+
+
+
+let laptopSpecifications= {
+    Brand:"HP",
+    Model:"hp15s 15inch",
+    Processor:"Ryzen 5700u",
+    RamOptions:{
+        1:"8gb",
+        2:"16gb"
+    },
+    KeyBoardType:"Chicklet keyboard with backlit",
+    Rom:"500gb",
+
+
+}
+
+console.log(laptopSpecifications)
