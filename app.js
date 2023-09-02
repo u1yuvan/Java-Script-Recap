27754b19fa627c214c8bb06c2b76a262fd4f8c39
u1yuvan <yuvanraj700@gmail.com>
2023-09-02 11:52:07 +0530

    JavaScript Variables

---
+++ app.js
@@ -0,0 +1,13 @@
+// VARIABLE NAMES 
+// var - old way of saving variables
+// let - modern way of saving varables(we can modify or edit value any time)
+// const - we can't change the value (permanent value we can't edit or modify)
+let name = "yuvan";
+name ="YUVAN"
+const DOB ="10-10-2002"//we can use const variable like this permanent value
+let msg = "hello my name is " + name + " im Building JS i was born in "
+// alert
+alert(msg);
+
+// logging
+console.log(msg, DOB);
